e1473c1f9991c254e478e1141e1f87ac4c93dd6b
Alever <759636538@qq.com>
2025-11-03 12:44:09 +0800

    [update] Update global.d.ts.

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -1,8 +1,10 @@
-import 'vue'
+// src/types/global.d.ts
 import type * as apiType from '@/axios/api'
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
         $api: typeof apiType
     }
 }
+
+export { } // ✅ 必须加上，否则会被当作 script 文件
