22ca022003c1b75023dbcb21ab0d629e2185b922
Hao Liang <hao.liang@stu.pku.edu.cn>
2025-11-18 15:02:16 +0800

    add prompts registry to backend (#8)
    * add prompts backend
    
    * add prompts backend

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -4,11 +4,13 @@ from .endpoints import datasets
 from .endpoints import operators
 from .endpoints import tasks
 from .endpoints import pipelines
+from .endpoints import prompts
 api_router = APIRouter()
 # api_router.include_router(health.router, prefix="/health")
 api_router.include_router(datasets.router, prefix="/datasets")
 api_router.include_router(operators.router, prefix="/operators")
 api_router.include_router(tasks.router, prefix="/tasks")
 api_router.include_router(pipelines.router, prefix="/pipelines")
+api_router.include_router(prompts.router, prefix="/prompts")
 # api_router.include_router(models.router, prefix="/models")
 # api_router.include_router(inference.router, prefix="/inference")
