186362fb2052733b33cea055fb996af676cda3c3
scuuy <912074188@qq.com>
2025-12-14 16:48:05 +0800

    change pipeline config file 'pipeline_registry.yaml' into 2 files: 'pipeline_registry.json' & 'pipeline_execution.json'

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,9 +5,10 @@ class Settings(BaseSettings):
     ENV: str = "dev"
     DATA_REGISTRY: str = "data/data_registry.yaml" #
     TASK_REGISTRY: str = "data/task_registry.yaml"
-    PIPELINE_REGISTRY: str = "data/pipeline_registry.yaml"
+    PIPELINE_REGISTRY: str = "data/pipeline_registry.json"
     SERVING_REGISTRY: str = "data/serving_registry.yaml"
     DATAFLOW_CORE_DIR: str = "data/dataflow_core"
     OPS_JSON_PATH: str = "data/ops.json"  # op information cache
+    PIPELINE_EXECUTION_PATH: str = "data/pipeline_execution.json"
 
 settings = Settings()
